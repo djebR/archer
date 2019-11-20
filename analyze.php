eb5026a016d7ffa10f39cc0079fde8b55a29c439
DJEBRI Ahmed El Amine <38659063+djebR@users.noreply.github.com>
2019-11-20 22:44:28 +0100

    Analyze one triple

--- analyze.php
+++ analyze.php
@@ -1,4 +1,6 @@
 <?php
+
+    header('Content-Type: application/json');
     $key = $_REQUEST['key'];
 
     if(file_exists("results/0_{$key}.json") && file_exists("results/1_{$key}.json")){
@@ -6,29 +8,45 @@
         $s0 = json_decode(file_get_contents("results/0_{$key}.json"));
         $s1 = json_decode(file_get_contents("results/1_{$key}.json"));
 
-        // analyse 0 -> 1 links
-        foreach ($s0 as $value) {
-            var_dump($value);
-            echo "<br/>";
+        $linked0 = 0;
+        $linked1 = 0;
+        $count = 0;
+
+        // This will contain couples of possible links between predicates: useful for ontology alignment purposes
+        $possibleLinks = array();
+
+        // analyse from -> to links
+        foreach ($s0 as $triple0) {
+            $boo = false;
+            foreach ($s1 as $triple1) {
+                if(strtolower($triple1->object) == strtolower($triple0->object)){
+                    $count += 1;
+                    $boo = true;
+                    $possibleLinks[] = array($triple0->predicate, $triple1->predicate);
+                }
+            }
+            if($boo) $linked0 += 1;
         }
 
-        // analyse 1 -> 0 links
-        foreach ($s1 as $value) {
-            var_dump($value);
-            echo "<br/>";
+        // analyse to -> from links
+        foreach ($s1 as $triple1) {
+            $boo = false;
+            foreach ($s0 as $triple0) {
+                if(strtolower($triple1->object) == strtolower($triple0->object)) {
+                    $linked1 += 1;
+                    break;
+                }
+            }
         }
+
     }
     else die();
 
+    $data = array(
+        "links" => $count,
+        "nodesFrom" => $linked0,
+        "nodesTo" => $linked1,
+        "possibleLinkedPred" => $possibleLinks
+    );
+    echo json_encode($data);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Results</title>
-</head>
-<body>
-    
-</body>
-</html>
--- mapper.php
+++ mapper.php
@@ -166,7 +166,7 @@ ob_end_flush();
                 $i += 1;
             }
 
-            echo "<td><a href='analyze.php?key={$key}' data-toggle='modal' data-target='#exampleModalCenter'>Analyze</a></td></tr>";
+            echo "<td><a class='analysis' href='#' data-key='{$key}' data-toggle='modal' data-target='#exampleModalCenter'>Analyze</a></td></tr>";
         }
 
     ?>
@@ -190,10 +190,34 @@ ob_end_flush();
     </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <script>
+
+        $('.analysis').on('click', function(){
+            var that = $(this);
+            var key = $(this).data("key");
+            var firstTriple = $(this).parent().siblings('td').children('a:first');
+            var secondTriple = $(this).parent().siblings('td').children('a:last');
+
+            $.ajax({
+                type: "get",
+                url: "analyze.php?key=" + key,
+                data: "data",
+                dataType: "json",
+                success: function (response) {
+                    firstTriple.html(firstTriple.html() + "<span class='badge badge-danger float-right'>"+ response.nodesFrom + "</span>");
+                    secondTriple.html(secondTriple.html() + "<span class='badge badge-danger float-right'>"+ response.nodesTo + "</span>");
+                    that.html(that.html() + "<span class='badge badge-success float-right'>"+ response.links + "</span>")
+                }
+            });
+
+
+        });
+
+    </script>
 </body>
 </html>
 
