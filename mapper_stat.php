3ee46d6ad63dfdf3889f76d18fe2ee80ceaad45f
DJEBRI Ahmed El Amine <38659063+djebR@users.noreply.github.com>
2019-11-29 01:06:49 +0100

    gitignore

--- mapper_stat.php
+++ mapper_stat.php
@@ -150,7 +150,7 @@
 
 <head>
 
-    <title>Archer: Subjective Probability</title>
+    <title>Archer: Statistical Probability</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
